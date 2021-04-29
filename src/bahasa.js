233b86e43f0a00b8d9fba72bf8dcc3554925c125
Dodododoyu <83328544+Dodododoyu@users.noreply.github.com>
2021-04-29 03:36:32 -0300

    Update bahasa.js

--- src/bahasa.js
+++ src/bahasa.js
@@ -1,4 +1,4 @@
-const derechosignificado = (prefix, sender) => {
+const derechosignificado = (prefix, sender) => {Conjunto de principios, preceptos y reglas a que están sometidas las relaciones humanas en sociedad. / Facultad de hacer o exigir todo aquello que la ley o autoridad establece a favor de una persona. / Justo, fundado, razonable. / Acción que se tiene sobre una persona o cosa. / Justicia, razón.}
 	return `Conjunto de principios, preceptos y reglas a que están sometidas las relaciones humanas en sociedad. / Facultad de hacer o exigir todo aquello que la ley o autoridad establece a favor de una persona. / Justo, fundado, razonable. / Acción que se tiene sobre una persona o cosa. / Justicia, razón.`
 }
 
